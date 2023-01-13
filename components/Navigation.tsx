7068d6534788953a6fdf16148584dff1bece787b
krilooleh <mayorford777@gmail.com>
2023-01-13 10:39:01 +0200

    navbar v2 and first block

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,26 +1,32 @@
 import styles from '../styles/Navigation.module.css'
 import Link from "next/link"
+import * as ROUTES from "../ROUTES"
 
 function Navigation(){
     return(
         <section className={styles.section}>
-            <div className={styles.logo_container}></div>
+            <div className={styles.logo_container}>
+                <Link href={ROUTES.Home}>
+                    <img className={styles.logo_icon} src="logo.svg" alt="logo"/>
+                    <p>andriy <span className="color_text">auto.</span></p>
+                 </Link>
+            </div>
 
             <ul className={styles.nav_options}>
                 <li className={styles.nav_option}>
-                    <Link href="/">Всі авто</Link>
+                    <Link href={ROUTES.Home}>Всі авто</Link>
                 </li>
 
                 <li className={styles.nav_option}>
-                    <Link href="/order-a-car">Авто під замовлення</Link>
+                    <Link href={ROUTES.OrderACar}>Авто під замовлення</Link>
                 </li>
 
                 <li className={styles.nav_option}>
-                    <Link href="/about-us">Про нас</Link>
+                    <Link href={ROUTES.AboutUs}>Про нас</Link>
                 </li>
 
                 <li className={styles.nav_option}>
-                    <Link href="/contacts">Контакти</Link>
+                    <Link href={ROUTES.Contacts}>Контакти</Link>
                 </li>
             </ul>
         </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,23 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import BrandBlock from "../components/HomePage/BrandBlock"
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Andriy Auto</title>
-        <meta name="description" content="-" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/*<link rel="icon" href="/favicon.ico" />*/}
-      </Head>
-      <main>
-        <div >
-          <h1>Всі авто</h1>
-        </div>
-      </main>
-    </>
-  )
+
+    return (
+        <>
+            <Head>
+                <title>Andriy Auto</title>
+                <meta name="description" content="-"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                {/*<link rel="icon" href="/favicon.ico" />*/}
+            </Head>
+            <main>
+                <section className={styles.section}>
+                    <h1 id={styles.title}>ПРИВЕЗЕМО АВТО МРІЇ</h1>
+                </section>
+                <BrandBlock/>
+            </main>
+        </>
+    )
 }
