280d5e502ff9604a08dcae582859260c9750e6f9
Oleh-Kr <mayorford777@gmail.com>
2023-01-14 14:08:39 +0200

    change name of variable in all files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/HomePage/Home.module.css'
 import BrandBlock from "../components/HomePage/BrandBlock"
 
 export default function Home() {
